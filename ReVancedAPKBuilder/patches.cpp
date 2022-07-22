9db0a203b5e775681deb794ef0216d45e888e0f5
ptrtypedf <vlsdkfsadlkfj@gmail.com>
2022-07-22 01:40:40 -0400

    .

--- ReVancedAPKBuilder/patches.cpp
+++ ReVancedAPKBuilder/patches.cpp
@@ -10,32 +10,7 @@
 
 struct pInput
 {
-    char p1;
-    char p2;
-    char p3;
-    char p4;
-    char p5;
-    char p6;
-    char p7;
-    char p8;
-    char p9;
-    char p10;
-    char p11;
-    char p12;
-    char p13;
-    char p14;
-    char p15;
-    char p16;
-    char p17;
-    char p18;
-    char p19;
-    char p20;
-    char p21;
-    char p22;
-    char p23;
-    char p24;
-    char p25;
-    char p26;
+    char p[27];
 };
 
 enum Patches
@@ -105,7 +80,7 @@ void patchesExclude()
 {
     std::string patchCmds{ "java -jar revancedCLI.jar -a youtube.apk -c -o revanced.apk -b revancedPatches.jar -m revancedIntegrations.apk -e background-play -e exclusive-audio-playback -e codecs-unlock -e upgrade-button-remover -e tasteBuilder-remover -e minimized-playback-music -e hide-get-premium -e music-video-ads" };
 
-        pInput patchInput{};
+    pInput patchInput[26]{};
 
         conColor(10);
         std::cout << "\t\t|------------------------------------------------------------------|\n";
@@ -114,190 +89,190 @@ void patchesExclude()
 
         std::cout << "\nExclude [1] | ";
         std::cout << getPatchName(hideInfoCardSuggestions);
-        std::cin >> patchInput.p1;
+        std::cin >> patchInput->p;
 
-        if (patchInput.p1 == 'e' || patchInput.p1 == 'E')
+        if (patchInput->p[0] == 'e' || patchInput->p[0] == 'E')
             patchCmds += " -e hide-infocard-suggestions";
 
         std::cout << "Exclude [2] | ";
         std::cout << getPatchName(videoAds);
-        std::cin >> patchInput.p2;
+        std::cin >> patchInput->p[2];
 
-        if (patchInput.p2 == 'e' || patchInput.p2 == 'E')
+        if (patchInput->p[2] == 'e' || patchInput->p[2] == 'E')
             patchCmds += " -e video-ads";
 
         std::cout << "Exclude [3] | ";
         std::cout << getPatchName(generalAds);
-        std::cin >> patchInput.p3;
+        std::cin >> patchInput->p[3];
 
-        if (patchInput.p3 == 'e' || patchInput.p3 == 'E')
+        if (patchInput->p[3] == 'e' || patchInput->p[3] == 'E')
             patchCmds += " -e general-ads";
 
         std::cout << "Exclude [4] | ";
         std::cout << getPatchName(seekbarTapping);
-        std::cin >> patchInput.p4;
+        std::cin >> patchInput->p[4];
 
-        if (patchInput.p4 == 'e' || patchInput.p4 == 'E')
+        if (patchInput->p[4] == 'e' || patchInput->p[4] == 'E')
             patchCmds += " -e seekbar-tapping";
 
         std::cout << "Exclude [5] | ";
         std::cout << getPatchName(swipeControls);
-        std::cin >> patchInput.p5;
+        std::cin >> patchInput->p[5];
 
-        if (patchInput.p5 == 'e' || patchInput.p5 == 'E')
+        if (patchInput->p[5] == 'e' || patchInput->p[5] == 'E')
             patchCmds += " -e swipe-controls";
 
         std::cout << "Exclude [6] | ";
         std::cout << getPatchName(microgSupport);
-        std::cin >> patchInput.p6;
+        std::cin >> patchInput->p[6];
 
-        if (patchInput.p6 == 'e' || patchInput.p6 == 'E')
+        if (patchInput->p[6] == 'e' || patchInput->p[6] == 'E')
             patchCmds += " -e microg-support";
 
         std::cout << "Exclude [7] | ";
         std::cout << getPatchName(hdrAutoBrightness);
-        std::cin >> patchInput.p7;
+        std::cin >> patchInput->p[7];
 
-        if (patchInput.p7 == 'e' || patchInput.p7 == 'E')
+        if (patchInput->p[7] == 'e' || patchInput->p[7] == 'E')
             patchCmds += " -e hdr-auto-brightness";
 
-        if (patchInput.p7 == 'i' || patchInput.p7 == 'I')
+        if (patchInput->p[7] == 'i' || patchInput->p[7] == 'I')
             patchCmds += " -i hdr-auto-brightness";
 
         std::cout << "Exclude [8] | ";
         std::cout << getPatchName(autoRepeatByDefault);
-        std::cin >> patchInput.p8;
+        std::cin >> patchInput->p[8];
 
-        if (patchInput.p8 == 'e' || patchInput.p8 == 'E')
+        if (patchInput->p[8] == 'e' || patchInput->p[8] == 'E')
             patchCmds += " -e autorepeat-by-default";
 
         std::cout << "Exclude [9] | ";
         std::cout << getPatchName(customPlaybackSpeed);
-        std::cin >> patchInput.p9;
+        std::cin >> patchInput->p[9];
 
-        if (patchInput.p9 == 'e' || patchInput.p9 == 'E')
+        if (patchInput->p[9] == 'e' || patchInput->p[9] == 'E')
             patchCmds += " -e custom-playback-speed";
 
         std::cout << "Exclude [10] | ";
         std::cout << getPatchName(enableDebugging);
-        std::cin >> patchInput.p10;
-
-        if (patchInput.p10 == 'e' || patchInput.p10 == 'E')
+        std::cin >> patchInput->p[10];
+   
+        if (patchInput->p[10] == 'e' || patchInput->p[10] == 'E')
             patchCmds += " -e enable-debugging";
 
         std::cout << "Exclude [11] | ";
         std::cout << getPatchName(oldQualityLayout);
-        std::cin >> patchInput.p11;
+        std::cin >> patchInput->p[11];
 
-        if (patchInput.p11 == 'y' || patchInput.p11 == 'Y')
+        if (patchInput->p[11] == 'y' || patchInput->p[11] == 'Y')
             patchCmds += " -e old-quality-layout";
 
         std::cout << "Exclude [12] | ";
         std::cout << getPatchName(hideCastButton);
-        std::cin >> patchInput.p12;
+        std::cin >> patchInput->p[12];
 
-        if (patchInput.p12 == 'e' || patchInput.p12 == 'E')
+        if (patchInput->p[12] == 'e' || patchInput->p[12] == 'E')
             patchCmds += " -e hide-cast-button";
 
         std::cout << "Exclude [13] | ";
         std::cout << getPatchName(amoled);
-        std::cin >> patchInput.p13;
+        std::cin >> patchInput->p[13];
 
-        if (patchInput.p13 == 'e' || patchInput.p13 == 'E')
+        if (patchInput->p[13] == 'e' || patchInput->p[13] == 'E')
             patchCmds += " -e amoled";
 
         std::cout << "Exclude [14] | ";
         std::cout << getPatchName(hideAutoplayButton);
-        std::cin >> patchInput.p14;
+        std::cin >> patchInput->p[14];
 
-        if (patchInput.p14 == 'e' || patchInput.p14 == 'E')
+        if (patchInput->p[14] == 'e' || patchInput->p[14] == 'E')
             patchCmds += " -e hide-autoplay-button";
 
         std::cout << "Exclude [15] | ";
         std::cout << getPatchName(minimizedPlayback);
-        std::cin >> patchInput.p15;
+        std::cin >> patchInput->p[15];
 
-        if (patchInput.p15 == 'e' || patchInput.p15 == 'E')
+        if (patchInput->p[15] == 'e' || patchInput->p[15] == 'E')
             patchCmds += " -e minimized-playback";
 
         std::cout << "Exclude [16] | ";
         std::cout << getPatchName(premiumHeading);
-        std::cin >> patchInput.p16;
+        std::cin >> patchInput->p[16];
 
-        if (patchInput.p16 == 'e' || patchInput.p16 == 'E')
+        if (patchInput->p[16] == 'e' || patchInput->p[16] == 'E')
             patchCmds += " -e premium-heading";
 
         std::cout << "Exclude [17] | ";
         std::cout << getPatchName(customBranding);
-        std::cin >> patchInput.p17;
+        std::cin >> patchInput->p[17];
 
-        if (patchInput.p17 == 'e' || patchInput.p17 == 'E')
+        if (patchInput->p[17] == 'e' || patchInput->p[17] == 'E')
             patchCmds += " -e custom-branding";
 
         std::cout << "Exclude [18] | ";
         std::cout << getPatchName(enableWideSearchbar);
-        std::cin >> patchInput.p18;
+        std::cin >> patchInput->p[18];
 
-        if (patchInput.p18 == 'e' || patchInput.p18 == 'E')
+        if (patchInput->p[18] == 'e' || patchInput->p[18] == 'E')
             patchCmds += " -e enable-wide-searchbar";
 
         std::cout << "Exclude [19] | ";
         std::cout << getPatchName(returnYoutubeDislikes);
-        std::cin >> patchInput.p19;
+        std::cin >> patchInput->p[19];
 
-        if (patchInput.p19 == 'e' || patchInput.p19 == 'E')
+        if (patchInput->p[19] == 'e' || patchInput->p[19] == 'E')
             patchCmds += " -e return-youtube-dislike";
 
         std::cout << "Exclude [20] | ";
         std::cout << getPatchName(disableFullscreenPanels);
-        std::cin >> patchInput.p20;
+        std::cin >> patchInput->p[20];
 
-        if (patchInput.p20 == 'e' || patchInput.p20 == 'E')
+        if (patchInput->p[20] == 'e' || patchInput->p[20] == 'E')
             patchCmds += " -e disable-fullscreen-panels";
 
         std::cout << "Exclude [21] | ";
         std::cout << getPatchName(hideShortsButton);
-        std::cin >> patchInput.p21;
+        std::cin >> patchInput->p[21];
 
-        if (patchInput.p21 == 'e' || patchInput.p21 == 'E')
+        if (patchInput->p[21] == 'e' || patchInput->p[21] == 'E')
             patchCmds += " -e hide-shorts-button";
 
         std::cout << "Exclude [22] | ";
         std::cout << getPatchName(disableCreateButton);
-        std::cin >> patchInput.p22;
+        std::cin >> patchInput->p[22];
 
-        if (patchInput.p22 == 'e' || patchInput.p22 == 'E')
+        if (patchInput->p[22] == 'e' || patchInput->p[22] == 'E')
             patchCmds += " -e disable-create-button";
 
         std::cout << "Exclude [23] | ";
         std::cout << getPatchName(hideWatermark);
-        std::cin >> patchInput.p23;
+        std::cin >> patchInput->p[23];
 
-        if (patchInput.p23 == 'e' || patchInput.p23 == 'E')
+        if (patchInput->p[23] == 'e' || patchInput->p[23] == 'E')
             patchCmds += " -e hide-watermark";
 
         std::cout << "Exclude [24] | ";
         std::cout << getPatchName(sponsorblock);
-        std::cin >> patchInput.p24;
+        std::cin >> patchInput->p[24];
 
-        if (patchInput.p24 == 'e' || patchInput.p24 == 'E')
+        if (patchInput->p[24] == 'e' || patchInput->p[24] == 'E')
             patchCmds += " -e sponsorblock";
 
         std::cout << "Exclude [25] | ";
         std::cout << getPatchName(forceVP9Codec);
-        std::cin >> patchInput.p25;
+        std::cin >> patchInput->p[25];
 
-        if (patchInput.p25 == 'e' || patchInput.p25 == 'E')
+        if (patchInput->p[25] == 'e' || patchInput->p[25] == 'E')
             patchCmds += " -e force-vp9-codec";
 
-        if (patchInput.p25 == 'i' || patchInput.p25 == 'I')
+        if (patchInput->p[25] == 'i' || patchInput->p[25] == 'I')
             patchCmds += " -i force-vp9-codec";
 
         std::cout << "Exclude [26] | ";
         std::cout << getPatchName(experimental);
-        std::cin >> patchInput.p26;
+        std::cin >> patchInput->p[26];
 
-        if (patchInput.p26 == 'i' || patchInput.p26 == 'I')
+        if (patchInput->p[26] == 'i' || patchInput->p[26] == 'I')
             patchCmds += " --experimental";
 
     conColor(12);
